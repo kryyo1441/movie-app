1c3213e1c0eced0be3b2750b9d6a8f6da8289cdb
zelli17 <aayushnair321@gmail.com>
2025-05-17 19:05:05 +0530

    Page routing and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import Favorites from './pages/Favorites'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
+    <div>
+      <NavBar></NavBar>
+    
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="favorites" element={<Favorites/>}></Route>
+        </Routes>
+      </main>
+      </div>
     </>
   )
 }
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favourites (){
+    return (
+        <div>
+            <h1>Favourites</h1>
+            <p>This is the Favourites page.</p>
+        </div>
+    )
+}
+
+export default Favourites
